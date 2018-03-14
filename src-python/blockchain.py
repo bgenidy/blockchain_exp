f7e72ac5001f8ca88310dda19f688437f9bda148
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2018-03-13 23:13:17 -0700

    blockchain.py preliminary skeleton filled

--- src-python/blockchain.py
+++ src-python/blockchain.py
@@ -1,22 +1,67 @@
+import hashlib
+import json
+from time import time
+
 class BlockChain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # creates a new block and adds it to the chain
-        pass
+        self.new_block(previous_hash='1', proof=100)
 
-    def new_transactions(self):
-        # adds a new transaction to the list of transactions
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the blockchain
+
+        :param proof: <int> proof given by proof of work algorithm
+        :param previous_hash: (optional) <str> hash of previous block
+        :return: <dict> new block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # reset current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transactions(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined block
+
+        :param sender: <str> address of the sender
+        :param recipient: <str> address of the recipient
+        :param amount: <int> amount
+        :return: <int> index of the block holding this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount':amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-        # hashes a block
-        pass
+        """
+        Creates a sha-256 hash of a block
+
+        :param block: <dict> block
+        :return: <str>
+        """
+
+        # ensure dictionary is ordered for consistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # returns the last block in the chain
-        pass
+        return self.chain[-1]
