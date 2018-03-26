3bb0a3c2a12b02c3fcb04bc5dd49a121861df967
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2018-03-26 16:00:12 -0700

    preliminary methods defined in header and implemented the constructor

--- src-c++/Blockchain.cpp
+++ src-c++/Blockchain.cpp
@@ -1,2 +1,60 @@
 
 #include "Blockchain.hpp"
+
+Blockchain::Blockchain() {
+    current_transactions = json::array();
+    chain = json::array();
+    nodes = json::array();
+}
+
+void Blockchain::registerNode(string address) {
+    // TODO validate that address is a properly formatted address
+    long sz = nodes.size();
+
+    for (long i = 0; i < sz; i++) {
+        if (nodes[i] == address) {
+            return; // already exists
+        }
+    }
+
+    nodes[sz] = address;
+}
+
+bool Blockchain::validChain(json chain) {
+    // TODO
+    return false;
+}
+
+bool Blockchain::resolveConflicts() {
+    // TODO
+    return false;
+}
+
+json Blockchain::newBlock(int proof, string previous_hash) {
+    // TODO
+    return nullptr;
+}
+
+int Blockchain::newTransaction(string sender, string recipient, json amount) {
+    // TODO
+    return 0;
+}
+
+json Blockchain::lastBlock() {
+    return chain[chain.size()-1];
+}
+
+int Blockchain::proofOfWork(json lastBlock) {
+    // TODO
+    return -1;
+}
+
+static string Blockchain::hash(json block) {
+    // TODO
+    return nullptr;
+}
+
+static bool Blockchain::validProof(int lastProof, int proof, string lastHash) {
+    // TODO
+    return false;
+}
--- src-c++/Blockchain.hpp
+++ src-c++/Blockchain.hpp
@@ -2,9 +2,30 @@
 #ifndef SRC_C_BLOCKCHAIN_HPP
 #define SRC_C_BLOCKCHAIN_HPP
 
+#include "json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
 
 class Blockchain {
-// TODO
+private:
+    json current_transactions;
+    json chain;
+    json nodes;
+
+public:
+    Blockchain();
+    void registerNode(string address);
+    bool validChain(json chain);
+    bool resolveConflicts();
+    json newBlock(int proof, string previous_hash);
+    int newTransaction(string sender, string recipient, json amount);
+    json lastBlock();
+    int proofOfWork(json lastBlock);
+
+
+    static string hash(json block);
+    static bool validProof(int lastProof, int proof, string lastHash);
 };
 
 
