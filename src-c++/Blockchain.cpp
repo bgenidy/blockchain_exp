c6ea314503046c1ce8f603480d5106075bc98d71
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2018-03-31 14:06:10 -0700

    new block, new transaction, and proof of work methods implemented. (and removed my invalid c++ syntax :) )

--- src-c++/Blockchain.cpp
+++ src-c++/Blockchain.cpp
@@ -1,23 +1,26 @@
 
+#include <iostream>
+#include <ctime>
 #include "Blockchain.hpp"
 
 Blockchain::Blockchain() {
     current_transactions = json::array();
     chain = json::array();
     nodes = json::array();
+
+    newBlock(1, "100");
 }
 
 void Blockchain::registerNode(string address) {
     // TODO validate that address is a properly formatted address
-    long sz = nodes.size();
 
-    for (long i = 0; i < sz; i++) {
-        if (nodes[i] == address) {
+    for (auto& element: nodes) {
+        if (element == address) {
             return; // already exists
         }
     }
 
-    nodes[sz] = address;
+    nodes.push_back(address);
 }
 
 bool Blockchain::validChain(json chain) {
@@ -31,30 +34,53 @@ bool Blockchain::resolveConflicts() {
 }
 
 json Blockchain::newBlock(int proof, string previous_hash) {
-    // TODO
-    return nullptr;
+    json block = json::object();
+    block["index"] = chain.size() + 1;
+    time_t now = time(0);
+    block["timestamp"] = ctime(&now);
+    block["transactions"] = current_transactions;
+    block["proof"] = proof;
+    block["previous_hash"] = previous_hash.empty() ? hash(chain[chain.size()-1]) : previous_hash;
+
+    current_transactions.clear();
+
+    chain.push_back(block);
+    return block;
 }
 
 int Blockchain::newTransaction(string sender, string recipient, json amount) {
-    // TODO
-    return 0;
+    json transaction = json::object();
+    transaction["sender"] = sender;
+    transaction["recipient"] = recipient;
+    transaction["amount"] = amount;
+
+    current_transactions.push_back(transaction);
+    int index = lastBlock()["index"];
+    return index + 1;
 }
 
 json Blockchain::lastBlock() {
     return chain[chain.size()-1];
 }
 
-int Blockchain::proofOfWork(json lastBlock) {
-    // TODO
-    return -1;
+int Blockchain::proofOfWork(json last_block) {
+    int last_proof = lastBlock()["proof"];
+    string last_hash = hash(last_block);
+
+    int proof = 0;
+    while (validProof(last_proof, proof, last_hash) == false) {
+        proof++;
+    }
+
+    return proof;
 }
 
-static string Blockchain::hash(json block) {
+string Blockchain::hash(json block) {
     // TODO
-    return nullptr;
+    return "";
 }
 
-static bool Blockchain::validProof(int lastProof, int proof, string lastHash) {
+bool Blockchain::validProof(int lastProof, int proof, string lastHash) {
     // TODO
     return false;
 }
